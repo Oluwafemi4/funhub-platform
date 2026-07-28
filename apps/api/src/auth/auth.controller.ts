8758f069543df7337f6693cbd7f38304957904f1
Olabisi Oluwafemi Samson <olabisifemi4@gmail.com>
2026-07-28 17:35:16 +0100

    Update auth.controller.ts

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { RegisterUserDto } from './dto/register-user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Post('register')
+  register(@Body() registeruserDto: RegisterUserDto) {
+
+  }
+}
