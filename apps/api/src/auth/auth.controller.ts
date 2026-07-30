1dd1693c65abfcf85ab23cd541eeb5f5f8e0f334
Olabisi Oluwafemi Samson <olabisifemi4@gmail.com>
2026-07-30 17:47:48 +0100

    Implement secure user registration

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { RegisterUserDto } from './dto/register-user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  @Post('register')
-  register(@Body() registeruserDto: RegisterUserDto) {
+  constructor(private readonly authService: AuthService) {}
 
+  @Post('register')
+  register(@Body() registerUserDto: RegisterUserDto) {
+  return this.authService.register(registerUserDto);
   }
 }
--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { RegisterUserDto } from './dto/register-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+
+async register(registerUserDto: RegisterUserDto) {
+ const hashedPassword = await bcrypt.hash(registerUserDto.password, 10);
+ 
+ const existingUser = await this.prisma.user.findUnique({
+  where: {
+    email: registerUserDto.email,
+  },
+});
+
+if (existingUser) {
+  throw new ConflictException('An account with this email already exists.');
+}
+
+ const user = await this.prisma.user.create({
+  data: {
+    firstName: registerUserDto.firstName,
+    lastName: registerUserDto.lastName,
+    email: registerUserDto.email,
+    passwordHash: hashedPassword,
+  },
+});
+
+return user;
+}
+}
