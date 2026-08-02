2cc4d7a0969c5811ff5fd28260ba1e8551d24c37
Olabisi Oluwafemi Samson <olabisifemi4@gmail.com>
2026-08-02 20:34:29 +0100

    business controller

--- apps/api/src/business/business.controller.ts
+++ apps/api/src/business/business.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Post, Get, Body, Param, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Req, UseGuards } from '@nestjs/common';
 import {ApiBearerAuth } from '@nestjs/swagger';
 import { BusinessService } from './business.service';
 import { CreateBusinessDto } from './dto/create-business.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { UpdateBusinessDto } from './dto/update-business.dto';
 
 @Controller('business')
 export class BusinessController {
@@ -36,4 +37,20 @@ export class BusinessController {
   findOne(@Param('id') id: string) {
     return this.businessService.findOne(id);
   }
+@Patch(':id')
+@UseGuards(JwtAuthGuard)
+@ApiBearerAuth('JWT')
+update(
+  @Req() req,
+  @Param('id') id: string,
+  @Body() updateBusinessDto: UpdateBusinessDto,
+) {
+ 
+ console.log('Authenticated user:', req.user);
+  return this.businessService.update(
+    id,
+    req.user.userId,
+    updateBusinessDto,
+  );
+}
 }
--- apps/api/src/business/business.service.ts
+++ apps/api/src/business/business.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateBusinessDto } from './dto/create-business.dto';
+import { UpdateBusinessDto } from './dto/update-business.dto';
 
 @Injectable()
 export class BusinessService {
@@ -54,4 +55,32 @@ export class BusinessService {
 },
     });
   }
+
+
+async update(
+  id: string,
+  ownerId: string,
+  updateBusinessDto: UpdateBusinessDto,
+) {
+  const business = await this.prisma.business.findUnique({
+    where: {
+      id,
+    },
+  });
+
+ if (!business) {
+  throw new NotFoundException('Business not found');
+}
+
+if (business.ownerId !== ownerId) {
+  throw new ForbiddenException('You are not allowed to update this business');
+}
+
+  return this.prisma.business.update({
+    where: {
+      id,
+    },
+    data: updateBusinessDto,
+  });
+}
 }
